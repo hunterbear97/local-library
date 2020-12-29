bbc6110f8e6ce13ca90113d42e6851310d02751e
hunterbear97 <hlytle@student.neumont.edu>
2020-12-29 12:16:48 -0700

    First version of application moved into github

---
+++ catalog/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    
+    list_display = ('book', 'status', 'due_back', 'id')
+    
+    fieldsets = (
+        (None, {
+            "fields": (
+                'book',
+                'imprint',
+                'id'
+            ),
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        })
+    )
+    
+
+
+# admin.site.register(Book)
+admin.site.register(Genre)
+# admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
